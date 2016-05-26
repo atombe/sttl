bc2b911c16d5aef1e3725efa86db3848b95490d1
atombe <atombe7@gmail.com>
2016-05-26 16:55:05 -0400

    Delete Macros.hpp

--- include/sttl/Macros.hpp
+++
@@ -1,8 +0,0 @@
-#define DECLARE_INTERFACE(InterfaceName)                            \
-  public:                                                           \
-    virtual ~InterfaceName = 0;                                     \
-  protected:                                                        \
-    InterfaceName() = default;                                      \
-    InterfaceName( const InterfaceName& ) = default;                \
-    InterfaceName& operator=( const InterfaceName& ) = default;     \
-  private:
